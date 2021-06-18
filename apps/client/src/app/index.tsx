a74cd7cac77a771c5dd7104bc09923212c42e66a
Corey Larson <coreyleelarson@gmail.com>
2021-06-18 17:27:12 -0500

    Reorganizes app component, moves typescript dependency to root, and sets workspace typescript setting.

--- apps/client/src/app/index.tsx
+++ apps/client/src/app/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ReactElement } from 'react';
+import logo from 'images/logo.svg';
 import './App.css';
 
-function App() {
+export const App = (): ReactElement => {
   return (
     <div className="App">
       <header className="App-header">
